e70eb4ce56974540bf8fd0b5c4db5b475cb41c96
Erik Rosolowsky <rosolowsky@ualberta.ca>
2019-10-29 19:52:36 +0000

    More sane approach to spatial masks

--- degas/postprocess.py
+++ degas/postprocess.py
@@ -42,7 +42,6 @@ def cleansplit(filename, galaxy=None,
                Vgalaxy = 300 * u.km / u.s,
                blorder=3, HanningLoops=0,
                maskfile=None,
-               maskblorder=11,
                edgeMask=True,
                weightCut=None,
                spectralSetup=None,
@@ -123,24 +122,6 @@ def cleansplit(filename, galaxy=None,
             spectralSetup = '12CO'
             filestr = '12co'
 
-    if maskfile is not None:
-        maskLookup = buildMaskLookup(maskfile)
-        shp = Cube.shape
-        spaxis = Cube.spectral_axis.to(u.Hz).value
-        data = Cube.filled_data[:].value
-        for y in np.arange(shp[1]):
-            for x in np.arange(shp[2]):
-                spectrum = data[:, y, x]
-                if np.any(np.isnan(spectrum)):
-                    continue
-                coords = Cube.world[:, y, x]
-                mask = maskLookup(coords[2].value,
-                                  coords[1].value,
-                                  spaxis)
-                spectrum = robustBaseline(spectrum, blorder=maskblorder,
-                                          baselineIndex=~mask)
-                data[:, y, x] = spectrum
-        Cube = SpectralCube(data, Cube.wcs, header=Cube.header)
     if spectralSetup == '13CO_C18O': 
         CEighteenO = Cube.with_spectral_unit(u.km / u.s,
                                              velocity_convention='radio',
@@ -180,15 +161,36 @@ def cleansplit(filename, galaxy=None,
         StartChan = ThisCube.closest_spectral_channel(V0 - Vgalaxy)
         EndChan = ThisCube.closest_spectral_channel(V0 + Vgalaxy)
 
-        # Rebaseline
-        # gbtpipe.Baseline.rebaseline(Galaxy + '_' + ThisLine + '.fits',
-        #                             baselineRegion=[slice(0,StartChan,1),
-        #                                             slice(EndChan,
-        #                                                   ThisCube.shape[0],1)],
-        #                             blorder=blorder)
-        # ThisCube = SpectralCube.read(Galaxy + '_' + ThisLine +
-        #                              '_rebase{0}'.format(blorder) + '.fits')
-        # Smooth
+        if maskfile is not None:
+            maskLookup = buildMaskLookup(maskfile)
+            shp = ThisCube.shape
+            spaxis = ThisCube.spectral_axis.to(u.Hz).value
+            data = ThisCube.filled_data[:].value
+            for y in np.arange(shp[1]):
+                for x in np.arange(shp[2]):
+                    spectrum = data[:, y, x]
+                    if np.any(np.isnan(spectrum)):
+                        continue
+                    coords = ThisCube.world[:, y, x]
+                    mask = maskLookup(coords[2].value,
+                                      coords[1].value,
+                                      spaxis)
+                    spectrum = robustBaseline(spectrum, blorder=maskblorder,
+                                              baselineIndex=~mask)
+                    data[:, y, x] = spectrum
+            ThisCube = SpectralCube(data, Cube.wcs, header=Cube.header)
+            ThisCube.write(Galaxy
+                           + '_' + ThisLine
+                           + '_rebase{0}.fits'.format(blorder), overwrite=True)
+        else:
+            gbtpipe.Baseline.rebaseline(Galaxy + '_' + ThisLine + '.fits',
+                                        baselineRegion=[slice(0,StartChan,1),
+                                                        slice(EndChan,
+                                                              ThisCube.shape[0],1)],
+                                        blorder=blorder)
+        ThisCube = SpectralCube.read(Galaxy + '_' + ThisLine +
+                                     '_rebase{0}'.format(blorder) + '.fits')
+        Smooth
         Kern = Kernel1D(array=np.array([0.5, 1.0, 0.5]))
         for i in range(HanningLoops):
             ThisCube.spectral_smooth(Kern)

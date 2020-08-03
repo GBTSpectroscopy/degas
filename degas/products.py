3ca8c6f691b6b6d15308228796638b53ea916623
Amanda Kepley <amanda@kepley.us>
2020-08-03 12:10:14 -0400

    updates to mask making and product code

--- degas/products.py
+++ degas/products.py
@@ -7,13 +7,16 @@ from astropy.io import fits
 from astropy import units as u
 
 
-def makeMap(cubeFile, maskFile='',maptype='peakIntensity',order=0):
+def makeMap(cubeFile, outDir, maskFile='',maptype='peakIntensity',order=0):
     """ Create peak intensity map from a cube
 
     Parameters
 
     cubeFile: str
        FITS filename of the cube
+
+    outDir: str
+        Directory in which to place the output FITS file
 
     maskFile: str 
         FITS filename of the mask. Should have same geometry as
@@ -38,18 +41,18 @@ def makeMap(cubeFile, maskFile='',maptype='peakIntensity',order=0):
 
     if maptype is 'peakIntensity':
         mymap = maskedCube.max(axis=0)
-        outname = cubeFile.replace('.fits','_peakInt.fits')
+        outname = os.path.basename(cubeFile).replace('.fits','_peakInt.fits')
 
     elif maptype is 'linewidth':
         mymap = maskedCube.linewidth_fwhm()
-        outname = cubeFile.replace('.fits','_linewidth.fits')
+        outname = os.path.basename(cubeFile).replace('.fits','_linewidth.fits')
     elif maptype is 'moment':
         mymap = maskedCube.moment(order=order)
-        outname = cubeFile.replace('.fits','_mom'+str(order)+'.fits')
+        outname = os.path.basename(cubeFile).replace('.fits','_mom'+str(order)+'.fits')
     else:
         print('Map type unknown. Options are peakIntensity, linewidth, moment')
         return
 
     mymap.meta['MASK'] = maskFile
-    mymap.write(outname,overwrite=True)
+    mymap.write(os.pathjoin(outDir,outname),overwrite=True)
 
--- scripts/make_masks.py
+++ scripts/make_masks.py
@@ -7,6 +7,7 @@
 
 import os
 from degas.masking import cubemask
+from degas.products import makeMap
 #import degas
 from astropy.table import Table, Column
 import glob
@@ -17,9 +18,13 @@ peakCut = 5.0
 lowCut = 3.0
 
 # set up the relevant directories
-maskDir = os.environ['MASKDIR'] 
-otherDataDir = os.environ['OTHERDATA']
+analysisDir = os.environ['ANALYSISDIR']
 scriptDir = os.environ['SCRIPTDIR']
+
+maskDir = os.path.join(analysisDir,'CO')
+
+otherDataDir = os.path.join(analysisDir,'ancillary_data')
+#otherDataDir = os.environ['OTHERDATA']
 
 # get list of galaxies in degas DR1
 degas_table = Table.read(os.path.join(scriptDir,"degas_base.fits"))
@@ -47,12 +52,16 @@ ovro_list = [ os.path.basename(image).split('.')[0].upper() for image in glob.gl
 
 for galaxy in degas_table[idx_dr1]:
 
+    generateMoments = True
+    outName = galaxy['NAME']+'_mask.fits'
+
     #for IC 0342 use Jialu's 12CO
     if galaxy['NAME'] == 'IC0342':
 
-        cubemask(os.path.join(otherDataDir,'jialu',
-                              'ic0342_regrid_12co_cube_Tmb_gauss15.fits'),
-                 galaxy['NAME']+'_mask.fits',
+        cubeFile = os.path.join(otherDataDir,'jialu',
+                                'ic0342_regrid_12co_cube_Tmb_gauss15.fits')
+        cubemask(cubeFile,
+                 outName,
                  outDir=maskDir,
                  peakCut=peakCut,lowCut=lowCut,
                  threeD=True,
@@ -60,15 +69,16 @@ for galaxy in degas_table[idx_dr1]:
                  minNchan=5.0)
 
         degas_table['MASK'][degas_table['NAME'] == galaxy['NAME']] = 'GBT'
-    
-        
+      
     ## use heracles first
     elif galaxy['NAME'] in heracles_list:
 
+        cubeFile = os.path.join(otherDataDir,'heracles',
+                                  galaxy['NAME']+'_heracles_gauss15.fits')
+
         if galaxy['NAME'] == 'NGC0337':
-            cubemask(os.path.join(otherDataDir,'heracles',
-                                  galaxy['NAME']+'_heracles_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=3.5,lowCut=2.0,
                      minBeamFrac=1.5,
@@ -76,65 +86,88 @@ for galaxy in degas_table[idx_dr1]:
                      threeD=True)
         else:
 
-            cubemask(os.path.join(otherDataDir,'heracles',
-                                  galaxy['NAME']+'_heracles_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+            cubeFile = os.path.join(otherDataDir,'heracles',
+                                  galaxy['NAME']+'_heracles_gauss15.fits')
+  
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,lowCut=lowCut,
                      minBeamFrac=1.5)
 
         degas_table['MASK'][degas_table['NAME'] == galaxy['NAME']] = 'HERACLES'
    
+
+
     ## use HERA data from Andreas next (everyHERACLES)
     elif galaxy['NAME'] in extra_hera_list:
     
         if galaxy['NAME'] == 'NGC3631':
 
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,lowCut=lowCut,
                      skipChan=[5])     
 
         elif galaxy['NAME'] == 'NGC4030':
             # this galaxy has a lot of fluffy low-level CO emission. By channel doesn't identify it as much as doing a three dimension cut for things that show up in multiple channels.
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=3.5,lowCut=2.0,
                      minBeamFrac=1.0,threeD=True,minNchan=3.0)
 
         elif galaxy['NAME'] == 'NGC3147':
             # this galaxy has a lot of fluffy low-level CO emission. By channel doesn't identify it as much as doing a three dimension cut for things that show up in multiple channels.
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=3.5,lowCut=2.0,
                      minBeamFrac=1.5,threeD=True,minNchan=5.0)   
+        
 
         elif galaxy['NAME'] == 'NGC4501':
             
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,lowCut=lowCut,
                      skipChan=[15])    
            
         elif galaxy['NAME'] == 'NGC4535':
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+            
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,lowCut=lowCut,
                      minBeamFrac=1.5)    
         else:
-            cubemask(os.path.join(otherDataDir,'co_from_andreas',
-                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+            cubeFile = os.path.join(otherDataDir,'co_from_andreas',
+                                  galaxy['NAME'].lower()+'_hera_co21.cube_fixed_10kms_gauss15.fits')
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,lowCut=lowCut)
 
@@ -145,17 +178,24 @@ for galaxy in degas_table[idx_dr1]:
         
         # single pointing case
         if galaxy['NAME'] == 'NGC4414':
-            cubemask(os.path.join(otherDataDir,'bima_song',
-                                  galaxy['NAME']+'_bima_gauss15_fixed.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+
+            cubeFile = os.path.join(otherDataDir,'bima_song',
+                                  galaxy['NAME']+'_bima_gauss15_fixed.fits')
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,
                      lowCut=lowCut)
         # multiple pointing cases
         else:
-            cubemask(os.path.join(otherDataDir,'bima_song',
-                                  galaxy['NAME']+'_bima_gauss15_fixed.fits'),
-                     galaxy['NAME']+'_mask.fits',
+
+            cubeFile = os.path.join(otherDataDir,'bima_song',
+                                  galaxy['NAME']+'_bima_gauss15_fixed.fits')
+
+            cubemask(cubeFile,
+                     outName,
                      outDir=maskDir,
                      peakCut=peakCut,
                      lowCut=2.0,
@@ -164,9 +204,12 @@ for galaxy in degas_table[idx_dr1]:
 
     ## use ovro next
     elif galaxy['NAME'] in ovro_list:
-        cubemask(os.path.join(otherDataDir,'temp_co',
-                              galaxy['NAME'].lower()+'.co.cmmsk_gauss15_fixed.fits'),
-                 galaxy['NAME']+'_mask.fits',
+
+        cubeFile = os.path.join(otherDataDir,'temp_co',
+                              galaxy['NAME'].lower()+'.co.cmmsk_gauss15_fixed.fits')
+
+        cubemask(cubeFile,
+                 outName,
                  outDir=maskDir,
                  peakCut=peakCut,lowCut=lowCut)       
 
@@ -174,11 +217,14 @@ for galaxy in degas_table[idx_dr1]:
 
     ##
     elif galaxy['NAME'] == 'NGC4038':
+        
+        cubeFile = os.path.join(otherDataDir,'ngc4038_from_chris',
+                              'ngc_4038_4039_7m_co10_gauss15.fits')
+
 
         ## ALMA DATA FROM CHRIS WILSON
-        cubemask(os.path.join(otherDataDir,'ngc4038_from_chris',
-                              'ngc_4038_4039_7m_co10_gauss15.fits'),
-                 galaxy['NAME']+'_mask.fits',
+        cubemask(cubeFile,
+                 outName,
                  outDir=maskDir,
                  peakCut=peakCut, lowCut=lowCut)
         
@@ -186,7 +232,31 @@ for galaxy in degas_table[idx_dr1]:
 
     
     else:
+        generateMoments=False
         print(galaxy['NAME']+" doesn't appear to have ancillary CO data.")
+
+        
+    if generateMoments:
+        # Mom0
+        makeMap(cubeFile,maskDir,
+                maskFile = os.path.join(maskDir,outName),
+                maptype='moment',order=0)
+        
+        # peakInt
+        makeMap(cubeFile,maskDir,
+                maskFile = os.path.join(maskDir,outName),
+                maptype='peakIntensity')
+
+        # moment 1
+        makeMap(cubeFile, maskDir,
+                maskFile = os.path.join(maskDir,outName),
+                maptype='moment',order=1)
+
+        # peak Vel -- yiqing is putting in code to do this.
+        #makeMap(cubeFile,maskDir,
+        #        maskFile = os.path.join(maskDir,outName),
+        #        maptype='peakVelocity')
+
     
 # write out degas data base table with the mask used.
 degas_table.write(os.path.join(scriptDir,"degas_base.fits"),overwrite=True)

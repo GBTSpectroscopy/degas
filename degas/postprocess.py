625f1d3e7951d34eb683e7c99660d14920e3ab07
Amanda Kepley <amanda@kepley.us>
2021-01-21 10:27:44 -0500

    added eta_mb calculation and application to cube

--- degas/postprocess.py
+++ degas/postprocess.py
@@ -14,6 +14,55 @@ from gbtpipe.Preprocess import buildMaskLookup
 from gbtpipe.Baseline import robustBaseline
 import warnings
 
+
+def calc_etamb(freq):
+
+    """
+
+    For a given input frequency, calculate and return the eta_mb value
+    for the GBT using David's equation from GBT Memo 302. The equation
+    is not included in the memo and provided by private communication:
+
+    eta_jupiter = 1.23 * eta_aperture + 0.005 * (nu - 60) - 0.00003*(nu-60)**2
+    where nu is in GHz.
+
+    It comes from a polynominal fit to nu and eta_jupiter using eta_aperture 
+    as a function of frequency.
+
+    This correction assumes that our sources are approximately the
+    size of Jupiter (43" diameter), which isn't a bad assumption for
+    extended molecular gas.
+
+    """
+    
+    import math
+    from astropy import constants as c
+
+    # surface error for GBT with optimal surface and excellent weather
+    esurf = 0.0235*u.cm # cm; GBT memo 302 says 230micron = 0.0230
+
+    # make the input frequency into a quantity if it isn't already
+    if not isinstance(freq,u.Quantity):
+        if freq > 1.0e9: # freq likely in Hz
+            freq = freq * u.Hz
+        elif freq < 200.0: # freq likely in GHz
+            freq = freq * u.GHz
+        else:
+            print("check units on input frequency. Should be either in GHz or Hz")
+    # convert frequency to GHz to use in fit
+    freq = freq.to(u.GHz)            
+
+    # calculate equivalent wavelength
+    wave = freq.to(u.cm, equivalencies=u.spectral())
+
+    # aperture efficiency for GBT using Ruze equation.
+    # 0.71 is the aperture efficiency of the GBT at lower frequency.
+    eta_a = 0.71 * math.exp(- ( 4 * math.pi * esurf / wave)**2)
+
+    # calculate eta_mb via a polynominal fit from GBT Memo 302
+    eta_mb = 1.23 * eta_a + 0.005*(freq.value-60) - 0.00003 * (freq.value - 60)**2
+    return eta_mb
+
 def circletrim(cube, wtsFile, x0, y0, weightCut=0.2):
     wtvals = np.squeeze(fits.getdata(wtsFile))
     badmask = ~(wtvals > (weightCut * wtvals.max()))
@@ -222,17 +271,25 @@ def cleansplit(filename, galaxy=None,
         # Smooth
         Kern = Kernel1D(array=np.array([0.5, 1.0, 0.5]))
         for i in range(HanningLoops):
-            ThisCube.spectral_smooth(Kern)
+            ThisCube.spectral_smooth(Kern) ## AAK: don't you have to put an output cube here?
             ThisCube = ThisCube[::2,:,:]
             
         # Spatial Smooth
         if spatialSmooth > 1.0:
             newBeam = Beam(major=ThisCube.beam.major * spatialSmooth,
                            minor=ThisCube.beam.minor * spatialSmooth)
-            ThisCube.convolve_to(newBeam)
+            ThisCube.convolve_to(newBeam)  ## AAK: don't you have to put an output cube here -- YES you do. CHECK WITH ERIC ABOUT THIS.
             smoothstr = '_smooth{0}'.format(spatialSmooth)
         else:
             smoothstr = ''
+
+        # apply eta_mb
+        ## see equations 2 and 3 in GBT memo 302.
+        ## GBT forward efficiency is 0.99 ~= 1.0. 
+        ## assumes that rest freq ~ observing freq, which should be okay for our sources.
+        freq = ThisCube.header['RESTFRQ'] * u.Hz
+        eta_mb = calc_etamb(freq)
+        ThisCube = ThisCube/eta_mb
 
         # Final Writeout
         ThisCube.write(Galaxy + '_' + ThisLine +

0393b657277ddd48ab1f8b7dc20b2e02938a7ee4
Amanda Kepley <amanda@kepley.us>
2022-01-20 17:35:00 -0500

    fixes to fiducial param table and improved stack trend plotting routine

--- scripts/make_stack_plots.py
+++ scripts/make_stack_plots.py
@@ -30,23 +30,36 @@ plot_dir =  os.path.join(os.environ['ANALYSISDIR'],'stack_IR6p1','stack_trends')
 binlist = ['radius','r25','mstar','ICO']
 
 mystyledict = {'int_intensity_sum_CO': 
-               {'marker':'o','color':'orange'}, 
+               {'marker':'o','color':'orange', 'label':'CO'}, 
                'int_intensity_sum_HCN': 
-               {'marker':'^','color':'green'}, 
+               {'marker':'^','color':'green', 'label':'HCN'}, 
                'int_intensity_sum_HCOp': 
-               {'marker':'s','color':'blue'}, 
+               {'marker':'s','color':'blue', 'label':'HCO+'}, 
                'int_intensity_sum_13CO': 
-               {'marker':'>','color':'red'}, 
+               {'marker':'>','color':'red', 'label':'13CO'}, 
                'int_intensity_sum_C18O': 
-               {'marker':'D','color': 'magenta'},
+               {'marker':'D','color': 'magenta', 'label':'C18O'},
                'ratio_HCN_CO':
-               {'marker':'^','color':'green'},
-               'ratio_HCOp_CO':
-               {'marker':'s','color':'blue'},
+               {'marker':'^','color':'green', 'label':r'HCN/$^{12}$CO'},
+                'ratio_HCOp_CO':
+               {'marker':'s','color':'blue', 'label':r'HCO+/$^{12}$CO'},
+               'ratio_ltir_mean_HCN':
+               {'marker':'^','color':'green', 'label':r'L$_{TIR}$/HCN (L$_\odot$ (K km s$^{-1}$ pc$^2$)$^{-1}$'},
+               'ratio_ltir_mean_HCOp':
+               {'marker':'^','color':'blue', 'label':r'L$_{TIR}$/HCO+ (L$_\odot$ (K km s$^{-1}$ pc$^2$)$^{-1}$'},
+               'ratio_HCOp_HCN':
+               {'marker':'s','color':'blue', 'label':r'HCO+/HCN'},
                'ratio_13CO_CO':
-               {'marker':'>','color':'red'},
+               {'marker':'>','color':'red', 'label':r'$^{13}$CO/$^{12}$CO'},
                'ratio_C18O_CO':
-               {'marker':'D','color': 'magenta'}}
+               {'marker':'D','color': 'magenta', 'label':r'$C^{18}$O/$^{12}$CO'},
+               'comass_mean': 
+               {'marker':'o', 'color':'orange','label':r'$\Sigma_{CO}$ (M$_\odot/pc^2$)'},
+               'mstar_mean':
+               {'marker':'o','color':'red','label':r'$\Sigma_*$ (M$_\odot/pc^2$)'},
+               'sfr_mean':
+               {'marker':'o','color':'blue','label':r'$\Sigma_{SFR}$ (M$_\odot/yr/pc^2$)'}}
+
 
 for bin_type in binlist:
 
@@ -67,15 +80,29 @@ for bin_type in binlist:
                 yaxislabel = r'Stacked Integrated Intensity (K km s$^{-1}$)',
                 xlog=xlog)
 
-
     plot_trends(stack, bin_type, plot_dir, degas_db,
                 ['comass_mean','mstar_mean','sfr_mean'],
                 bin_type + '_other',
                 factordict = {'sfr_mean': 1/1e10},
                 yaxislabel = r'Variable',
+                styledict = mystyledict,
                 xlog=xlog)
-            
+   
+    plot_trends(stack, bin_type, plot_dir, degas_db,
+                ['ratio_ltir_mean_HCN','ratio_ltir_mean_HCOp'],
+                bin_type + '_sfedense',
+                yaxislabel = r'L$_{TIR}$/HCN',
+                styledict = mystyledict,
+                xlog=xlog)         
 
+    plot_trends(stack, bin_type, plot_dir, degas_db,
+                ['ratio_HCN_CO','ratio_HCOp_CO'],
+                bin_type+'_fdense',
+                styledict = mystyledict,
+                yaxislabel = r'Dense gas fraction',
+                ylog=False,
+                xlog=xlog)
+    
     plot_trends(stack, bin_type, plot_dir, degas_db,
                 ['ratio_HCN_CO','ratio_HCOp_CO','ratio_13CO_CO','ratio_C18O_CO'],
                 bin_type+'_coratios',
@@ -83,11 +110,12 @@ for bin_type in binlist:
                 yaxislabel = r'Dense gas ratios',
                 ylog=False,
                 xlog=xlog)
-     
+
     plot_trends(stack, bin_type, plot_dir, degas_db,
                 ['ratio_HCOp_HCN'],
                 bin_type+'_hcop_hcn',
                 yaxislabel = r'Ratios',
+                styledict = mystyledict,
                 ylog=False,
                 xlog=xlog)
 
@@ -95,6 +123,7 @@ for bin_type in binlist:
                 ['ratio_13CO_C18O'],
                 'radius_13CO_C18O',
                 yaxislabel = r'Ratios',
+                styledict = mystyledict,
                 ylog=False,
                 xlog=xlog)
 
@@ -141,9 +170,24 @@ for bin_type in binlist:
                 ['comass_mean','mstar_mean','sfr_mean'],
                 bin_type + '_other',
                 factordict = {'sfr_mean': 1/1e10},
+                styledict = mystyledict,
                 yaxislabel = r'Variable',
                 xlog=xlog)
             
+    plot_trends(stack_pruned, bin_type, plot_dir, degas_db,
+                ['ratio_ltir_mean_HCN','ratio_ltir_mean_HCOp'],
+                bin_type + '_sfedense',
+                yaxislabel = r'L$_{TIR}$/HCN',
+                styledict = mystyledict,
+                xlog=xlog)         
+
+    plot_trends(stack_pruned, bin_type, plot_dir, degas_db,
+                ['ratio_HCN_CO','ratio_HCOp_CO'],
+                bin_type+'_fdense',
+                styledict = mystyledict,
+                yaxislabel = r'Dense gas fraction',
+                ylog=False,
+                xlog=xlog)
 
     plot_trends(stack_pruned, bin_type, plot_dir, degas_db,
                 ['ratio_HCN_CO','ratio_HCOp_CO','ratio_13CO_CO','ratio_C18O_CO'],
@@ -157,6 +201,7 @@ for bin_type in binlist:
                 ['ratio_HCOp_HCN'],
                 bin_type+'_hcop_hcn',
                 yaxislabel = r'Ratios',
+                styledict = mystyledict,
                 ylog=False,
                 xlog=xlog)
 
@@ -164,5 +209,6 @@ for bin_type in binlist:
                 ['ratio_13CO_C18O'],
                 'radius_13CO_C18O',
                 yaxislabel = r'Ratios',
+                styledict = mystyledict,
                 ylog=False,
                 xlog=xlog)

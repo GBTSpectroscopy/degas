0c8a587718d944f2fcc736147b460bf4cda74cf2
Amanda Kepley <amanda@kepley.us>
2022-09-01 15:19:07 -0400

    added driver routine for fitting code

--- degas/analysis_fit.py
+++ degas/analysis_fit.py
@@ -1,5 +1,7 @@
 import numpy as np
 from astropy.table import Table
+import os
+import ipdb
 
 def calc_correlation_coeffs(stack,degas_db, release='DR1',corr_bins = ['r25','mstar','ICO'],corr_quants=['ratio_HCN_CO','ratio_ltir_mean_HCN'], outfile=None):
     '''
@@ -103,23 +105,87 @@ def calc_correlation_coeffs(stack,degas_db, release='DR1',corr_bins = ['r25','ms
     return(result_tab)
 
 
+def make_fit_table(stack, 
+                   bins = ['r25','mstar','ICO'],
+                   columns = ['ratio_HCN_CO','ratio_ltir_mean_HCN'],
+                   outfile = 'test.fits',
+                   exclude_gal = [],
+                   plotDir = None):
+    '''
+    
+    Purpose: run fitting code for different parameters and save
+    results to table
+
+    stack: input stack
+    bins: bins to use for x vals
+    columns: columns to use for y vals
+    outfile: fits table for results
+    exclude_gal (optional): galaxies to exclude from fit
+
+    Date        Programmer      Description of Changes
+    ----------------------------------------------------------------------
+    9/1/2022   A.A. Kepley     Original Code
+
+    '''
+    
+    # create results table
+    t = Table(names=('bin','column',
+                     'slope','slope_err','intercept','intercept_err',
+                     'chisq','AIC','BIC'),
+              dtype=('S20','S20',
+                     float, float,float,float,
+                     float,float,float),
+              meta=stack.meta)
+
+    # remove any galaxies we are excluding
+    if len(exclude_gal) > 0:
+        for bad_gal in exclude_gal:
+            idx = stack['galaxy'] != bad_gal
+            stack = stack[idx]
+        t.meta['EXCLUDE_GAL'] = exclude_gal
+
+    # do fit for each bin/column combination
+    for mybin in bins:
+
+        # select out the bins we want
+        idx = stack['bin_type'] == mybin
+        if len(stack[idx]) == 0:
+            print("Bin " + mybin+ " not found in stack. Skipping")
+            continue
+
+        # iterate over columns    
+        for mycol in columns: 
+            if not mycol in stack.columns:
+                print("Column "+mycol+" not in stack. Skipping")
+                continue
+            myresult = fit_trend(stack[idx],bin_type=mybin,stack_col=mycol, plotDir=plotDir)
+
+            t.add_row([mybin,mycol,
+                       myresult.params['slope'].value,
+                       myresult.params['slope'].stderr,
+                       myresult.params['intercept'].value,
+                       myresult.params['intercept'].stderr,
+                       myresult.chisqr,
+                       myresult.aic,
+                       myresult.bic])
+
+    # write out results
+    t.write(outfile,overwrite=True)
+
+    return t
+    
+    
                                 
 def fit_trend(stack, 
               bin_type='r25', 
-              stack_col = 'ratio_HCN_CO', 
-              exclude_gal = ''):
+              stack_col = 'ratio_HCN_CO',
+              plotDir = None):
     '''
 
     Purpose: fit a linear regression to a subset of data from a stack
 
     TODO: 
-    -- deal with log errors.
-    -- how to save the models.
-        -- I could just save the m and x to a file? and perhaps the fit result?
-        -- from the lmfit documentation it looks like saving the model is a bit of a hassle and not guaranteed to load in different versions of python.
-        -- do i want to have a driver function that does a bunch of these and saves to an astropy table??
-    -- how to exclude galaxies from just one fit?? maybe move this up to driver code?? I may want to fit each galaxy at some point. Although that seems weird.
-    
+    -- save plots for diagnostic purposes??
 
 
     Date        Programmer      Description of Changes
@@ -134,11 +200,11 @@ def fit_trend(stack,
     import matplotlib.pyplot as plt
 
     if (stack_col+'_uplim' in stack.columns) and (stack_col+'_lolim' in stack.columns):
-        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_uplim'] == False) & (stack[stack_col+'_lolim'] == False) & (stack['galaxy'] != exclude_gal)
+        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_uplim'] == False) & (stack[stack_col+'_lolim'] == False) 
     elif (stack_col+'_uplim' in stack.columns):
-        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_uplim'] == False) & (stack['galaxy'] != exclude_gal)
+        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_uplim'] == False)  
     elif (stack_col+'_lolim' in stack.columns):
-        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_lolim'] == False) & (stack['galaxy'] != exclude_gal)
+        idx = (stack['bin_type'] == bin_type) & (stack[stack_col+'_lolim'] == False) 
         
 
     if bin_type == 'r25':
@@ -148,26 +214,42 @@ def fit_trend(stack,
 
     yvals = np.log10(np.array(stack[stack_col][idx]))
     
-    ## TODO: can't just take log values of errors.
-    yvals_err = np.log10(np.array(stack[stack_col+"_err"][idx]))
+    ## percent errors = errors in log space
+    yvals_err = stack[stack_col+"_err"][idx]/stack[stack_col][idx]
     yvals_weights = 1/(yvals_err**2)
+
+    mygals = stack['galaxy'][idx]
 
     mod = LinearModel(nan_policy='omit')
     params_mod = mod.guess(yvals,x=xvals)
 
+    #modresult = mod.fit(yvals,params=params_mod,x=xvals,verbose=True,
+    #   weights = yvals_weights)
+
     modresult = mod.fit(yvals,params=params_mod,x=xvals,verbose=True)
 
+    print("\n\n")
+    print("**************************************************")
+    print("Fit results for " + bin_type + " vs. " + stack_col)
     print(modresult.fit_report())
 
     plt.clf()
-    plt.scatter(xvals, yvals)
-    plt.plot(xvals,modresult.best_fit,label='best fit')
+
+    gallist = np.unique(mygals)
+    for gal in gallist:
+        plt.errorbar(xvals[gal == mygals], yvals[gal == mygals],
+                     yerr = yvals_err[gal==mygals],
+                     label=gal)
+
+    plt.plot(xvals,modresult.best_fit,label='best fit',color='darkgray',linewidth=3.0)
     plt.xlabel(bin_type)
     plt.ylabel(stack_col)
     plt.legend()
-    
-    ## TODO: to get value of fit parameter and error
-    ## modresult.params['slope'].value
-    ## modresult.params['slope'].stderr
+        
+    if plotDir:
+        if not os.path.exists(plotDir):
+            os.mkdir(plotDir)
+        plt.savefig(os.path.join(plotDir,bin_type + "_" + stack_col + "_fit.png"))
+        plt.savefig(os.path.join(plotDir,bin_type + "_" + stack_col + "_fit.pdf"))
 
     return modresult
--- scripts/do_fit_calc.py
+++ scripts/do_fit_calc.py
@@ -1,6 +1,6 @@
 from astropy.table import Table
 import os
-from degas.analysis_fit import fit_trend
+from degas.analysis_fit import fit_trend,make_fit_table
 
 release = 'IR6p1'
 
@@ -8,6 +8,9 @@ stack_dir = os.path.join(os.environ['ANALYSISDIR'],'stack_'+release)
 
 stack = Table.read(os.path.join(stack_dir,'stack_IR6p1_mom1_pruned.fits'))
 
-test = fit_trend(stack,bin_type='r25',stack_col='ratio_HCN_CO',exclude_gal='NGC4414')
+myresult = make_fit_table(stack,outfile=os.path.join(stack_dir,'stack_fits.fits'),plotDir=os.path.join(stack_dir,'fit_plots'))
 
-test2 = fit_trend(stack,bin_type='mstar',stack_col='ratio_HCN_CO')
+myresult_noNGC4414 = make_fit_table(stack,outfile=os.path.join(stack_dir,'stack_fits_noNGC4414.fits'),plotDir=os.path.join(stack_dir,'fit_plots_noNGC4414'),exclude_gal=['NGC4414'])
+
+#test2 = make_fit_table(stack,outfile=os.path.join(stack_dir,'stack_fits_noNGC4414.fits'),exclude_gal=['NGC4414'])
+
